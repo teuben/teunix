894716bfc559f1c47c39927717b0232498ecd513
Peter Teuben <teuben@gmail.com>
2018-06-05 08:36:40 -0400

    add java example

---
+++ Test/hello.java
@@ -0,0 +1,6 @@
+// the name of the class will be the name of the file that javac creates. go figure.
+class hello {
+    static public void main( String args[] ) {
+	System.out.println( "Hello World!" );
+    }
+}
