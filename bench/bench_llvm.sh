ac052c5dcf15d7fa1fc44e5856c8ca014cd6f999
Peter Teuben <teuben@gmail.com>
2021-04-07 07:53:49 -0400

    heavy benchmark

---
+++ bench/bench_llvm.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+#
+
+refresh=0
+local=0
+tgz=llvm-project.tar.gz
+
+#             simple keyword=value command line parser for bash - don't make any changing below
+for arg in $*; do
+  export $arg
+done
+
+
+if [ $refresh = 1 ]; then
+
+    git clone https://github.com/llvm/llvm-project.git
+    cd llvm-project
+    git checkout release/11.x
+    mkdir ./build
+    cd ..
+    mkdir llvm-project-tmpfs
+    sudo mount -t tmpfs -o size=10G,mode=1777 tmpfs ./llvm-project-tmpfs
+    cp -r llvm-project/* llvm-project-tmpfs
+    cd ./llvm-project-tmpfs/build
+    cmake -G Ninja \
+	  -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi;lldb;compiler-rt;lld" \
+	  -DCMAKE_BUILD_TYPE=Release ../llvm
+    /usr/bin/time cmake --build .
+else
+    echo refresh=0   No method as published in https://www.anandtech.com/show/16594/intel-3rd-gen-xeon-scalable-review/10
+fi
+
+if [ $local = 1 ]; then
+    git clone https://github.com/llvm/llvm-project.git
+    cd llvm-project
+    git checkout release/11.x
+    mkdir ./build
+    #
+    cmake -G Ninja \
+	  -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi;lldb;compiler-rt;lld" \
+	  -DCMAKE_BUILD_TYPE=Release ../llvm
+    /usr/bin/time cmake --build .
+else
+    echo local=0 no full git locally
+fi
+
+
+if [ -e $tgz ]; then
+    tar zxf llvm-project.tar.gz 
+    cd llvm-project
+    mkdir build
+    cd build
+    cmake -G Ninja \
+	  -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi;lldb;compiler-rt;lld" \
+	  -DCMAKE_BUILD_TYPE=Release ../llvm
+    time cmake --build .
+else
+    echo No tgz=$tgz  
+fi
+  
